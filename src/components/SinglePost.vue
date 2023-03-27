48c0278ec3bd614393f00200bd2b42f2760071e8
Antony <antony.jacques@laposte.net>
2023-03-27 14:41:53 +0200

    use props with computed

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-  <h3>{{post.title}}</h3></div>
+    <h3>{{post.title}}</h3>
+    <p>{{snippet}}</p>
+  </div>
 </template>
 
 <script>
+import {computed } from 'vue'
 export default {
-    props: ['post'],
-    setup(props){
-        console.log('single',props.post)
-    }
-}
+  props: ["post"],
+
+  setup(props) {
+    const snippet = computed(()=>{
+        return props.post.body.substring(0,10) + "...."
+    })
+
+    return { snippet }
+  }
+
+};
 </script>
 
 <style>
-
 </style>>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,11 +13,11 @@ export default {
   name: "HomeView",
   components: { PostList },
   setup() {
+    //imagine that we don't want the entire post.body
     const posts = [
-      {title: "welcome to the blog", body:'lorem ipsum', id: 1},
-      {title: "top 5 css tips", body:'loem ipsum', id: 2}
+      {title: "welcome to the blog", body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quidem maxime harum impedit laborum quo iusto exercitationem, a quisquam velit', id: 1},
+      {title: "top 5 css tips", body:'lorem ipsum', id: 2}
     ]
-
 
     return { posts };
   }
