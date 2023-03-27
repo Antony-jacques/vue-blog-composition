c3056d68c51a6c2a72aef712f3e9e765e55352f3
Antony <antony.jacques@laposte.net>
2023-03-27 12:13:41 +0200

    computed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,29 @@
 <template>
   <div class="home">
     <h1>ref</h1>
-    <p>
-      {{ firstUser.name }} {{ firstUser.age }}
-    </p> 
-    <div>
-      <button @click="updateUser">click</button>
-      <input type="text" v-model="name">
-    </div>
-
-    <h2>reactive</h2>
-    <p>{{ secondUser.name }} {{ secondUser.age }}</p>
-    <button @click="updateSecondUser">update user 2</button>
+    <p>{{search}}</p>
+    <input v-model="search"  type="text">
+    <li v-for="name in getName" :key="name">{{ name}}</li>
 
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 export default {
   name: "HomeView",
   setup() {
-    console.log("setup");
 
-    const firstUser = ref({
-      name: "Mario",
-      age: 20
+    const names= ref(["Mario","Luigi","Koopa","Toad","Bowser","Yoshi"])
+
+    const search = ref('')
+
+    const getName = computed(()=> {
+      return names.value.filter( name => name.includes(search.value))
     })
 
-    // reactive does not work with primitive like string, number...
-    const secondUser = reactive({
-        name: "Luigi",
-        age: 35
-    })
 
-    const updateUser = () => {
-      firstUser.value.name = "Luigi"
-      firstUser.value.age = 35
-    };
-
-    const updateSecondUser = ()=> {
-      secondUser.name = "toto"
-      secondUser.age = 60
-    }
-
-    return { firstUser, updateUser, secondUser, updateSecondUser };
+    return { names, search, getName };
   }
 };
 </script>
