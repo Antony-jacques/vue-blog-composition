84c69bed3097da48489ed64f4813579b4fb18344
Antony <antony.jacques@laposte.net>
2023-03-28 16:42:15 +0200

    fetch data using json server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,46 @@
 <template>
   <div class="home">
     <h1>home</h1>
-    <PostList :posts="posts" />
+    <div v-if="error">{{ error }}</div>
+    <div v-else-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>
+      <h3>loading</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed } from "vue";
+import { ref, onBeforeMount } from "vue";
 import PostList from "../components/postList.vue";
 export default {
   name: "HomeView",
   components: { PostList },
   setup() {
-    //imagine that we don't want the entire post.body
-    const posts = [
-      {
-        title: "welcome to the blog",
-        body:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quidem maxime harum impedit laborum quo iusto exercitationem, a quisquam velit",
-        id: 1
-      },
-      { title: "top 5 css tips", body: "lorem ipsum", id: 2 }
-    ];
+    const posts = ref([]);
+    const error = ref(null);
 
-    return { posts };
-  }
+    const loadData = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        console.log("here the data", data);
+
+        if(!data.ok){
+          throw new Error("no data available");
+        }
+        posts.value = await data.json();
+        console.log("here the posts", posts);
+      } catch (err) {
+        error.value = err.message;
+      }
+    };
+
+    onBeforeMount(() => {
+      loadData();
+    });
+
+    return { posts, error };
+  },
 };
 </script>
